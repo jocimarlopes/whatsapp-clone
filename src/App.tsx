ed72be6861d26794f19d47918cce37049c261f17
Victor Batalha <madivcb@gmail.com>
2020-10-18 22:43:34 -0300

    created router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { Router } from 'react-router-dom';
 
 import useThemeState from '@hooks/useThemeState';
 
-import GlobalStyles from './styles/global';
+import history from './services/history';
 import themes from './styles/themes';
+
+import GlobalStyles from './styles/global';
+import Routes from './routes';
 
 function App() {
   const [theme, setTheme] = useThemeState('main');
 
   return (
-    <ThemeProvider theme={themes[theme]}>
-      <button
-        type="button"
-        onClick={() => setTheme(theme === 'main' ? 'dark' : 'main')}
-      >
-        trocar tema
-      </button>
+    <Router history={history}>
+      <ThemeProvider theme={themes[theme]}>
+        <Routes />
 
-      <GlobalStyles />
-    </ThemeProvider>
+        <GlobalStyles />
+      </ThemeProvider>
+    </Router>
   );
 }
 
